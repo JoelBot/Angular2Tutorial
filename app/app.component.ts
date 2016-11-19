6b82528f37531dd40cea405b9432636bcecfb69a
JoelBot <Joel.Lonneker@gmail.com>
2016-11-19 16:47:54 -0500

    angular prework added

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello Angular!</h1>',
+  // templateUrl: 'hello-world.html',
+})
+// @Component({
+//   selector: 'joel-app',
+//   template: '<div class="form-control">Hello</div>'
+// })
+
+export class AppComponent { }
